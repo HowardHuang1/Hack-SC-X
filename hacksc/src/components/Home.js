48f1b731a7fa3cf397e7ce0b94fdaec1f5914074
Howard Huang <howardhuang2004@gmail.com>
2023-11-04 16:59:30 -0700

    create VRBuilder

---
+++ hacksc/src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Home.css"
+
+function Home() {
+  return (
+    <div>
+        Home
+
+        <div>
+            <p>This is the home screen</p>
+        </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ hacksc/src/components/VRBuilder.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./VRBuilder.css"
+
+function VRBuilder() {
+  return (
+    <div className="web-screen-partition">
+        <div className="web-left-section">
+            left section
+        </div>
+        <div className="web-right-section">
+            right section
+        </div>
+    </div>
+  )
+}
+
+export default VRBuilder
