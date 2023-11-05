de9533dbcc3beda73e620c62fe853dacb691c202
Howard Huang <howardhuang2004@gmail.com>
2023-11-04 21:55:53 -0700

    add threeJS rendering of coordinate axis

--- hacksc/src/components/ThreeJS.js
+++ hacksc/src/components/ThreeJS.js
@@ -1,11 +1,4 @@
 import React from 'react'
-import * as THREE from 'three';
-
-const renderer = new THREE.WebGLRenderer();
-
-renderer.setSize(window.innerWidth, window.innerHeight);
-
-document.body.appendChild(renderer.domElement);
 
 function ThreeJS() {
   return (
