d4509c84a6cac3cb653867b2665403439a114867
Howard Huang <howardhuang2004@gmail.com>
2023-11-04 17:13:16 -0700

    add user input button

--- hacksc/src/components/VRBuilder.js
+++ hacksc/src/components/VRBuilder.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./VRBuilder.css"
+import {
+    Box,
+    AccordionButton,
+    AccordionIcon,
+    Flex,
+    AccordionPanel,
+    Accordion,
+    AccordionItem,
+    Select,
+    Textarea,
+    Input,
+    IconButton,
+    Button,
+  } from "@chakra-ui/react";
 
 function VRBuilder() {
+    const [inputText, setInputText] = useState('');
+    const handleInputChange = (event) => {
+        setInputText(event.target.value);
+    }
+
   return (
     <div className="web-screen-partition">
         <div className="web-left-section">
@@ -9,6 +28,21 @@ function VRBuilder() {
         </div>
         <div className="web-right-section">
             right section
+            <div className="input-box">
+                <input
+                    type="text"
+                    placeholder="Enter text"
+                    value={inputText}
+                    onChange={handleInputChange}
+                />
+                <Box flex="30%" ml={2}>
+                    <Button w="100%" >Submit</Button>
+                </Box>
+                <div className="output-box">
+                    <p>User Input:</p>
+                    <div>{inputText}</div>
+                </div>
+            </div>
         </div>
     </div>
   )
