7cc2fe7cc4e97903b5bdcc9d538a87fc5b82c67f
Joseph Wang <josephwang@letu.edu>
2023-11-05 08:17:57 -0800

    Added functional Selenium code for Genie

--- selenium_test.py
+++ selenium_test.py
@@ -3,47 +3,40 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 
-generation_text = "2 story victorian house"
-multiplier = 0.8
-
+# generation_text = "2 story victorian house"
+sleep_factor = 1.5
 
 def generate_asset(text):
-    # textbox = driver.find_element(By.CLASS_NAME, "emptyText_c03d90")
-    # textbox.send_keys("/genie")
-    # sleep(1*multiplier)
-    # genie = driver.find_element(By.CLASS_NAME, "infoWrapper__1d10f")
-    # genie.click()
-    # sleep(1*multiplier)
-    # textbox = driver.find_element(By.CLASS_NAME, "emptyText_c03d90")
-    # textbox.send_keys(text)
-    # textbox.send_keys(Keys.ENTER)
-    # sleep(21)
+    # Enter prompt
+    textbox = driver.find_element(By.CLASS_NAME, "emptyText_c03d90")
+    textbox.send_keys("/genie")
+    sleep(1*sleep_factor)
+    genie = driver.find_element(By.CLASS_NAME, "infoWrapper__1d10f")
+    genie.click()
+    sleep(1*sleep_factor)
+    textbox = driver.find_element(By.CLASS_NAME, "emptyText_c03d90")
+    textbox.send_keys(text)
+    textbox.send_keys(Keys.ENTER)
+    sleep(21*sleep_factor)
 
+    # Click option 4
     buttons = driver.find_elements(By.CLASS_NAME, "emoji")
     buttons[-2].click()
-    sleep(6*multiplier)
+    sleep(6*sleep_factor)
 
-    if text == "download":
-        # action = webdriver.common.action_chains.ActionChains(driver)
+    # Download to downloads/
+    driver.switch_to.window(driver.window_handles[1])        
+    buttons = driver.find_elements(By.CLASS_NAME, "lucide")
+    buttons[0].click()
+    sleep(1*sleep_factor)
+    driver.switch_to.window(driver.window_handles[0])
 
-        # action.move_to_element_with_offset(first_img, -40, 750)
-        # action.click()
-        # action.perform()
-
-        # download_btn = driver.find_element(By.XPATH, '//div/*[name()="svg"][@class="lucide"]')
-        download_btn = driver.find_element(By.XPATH, '//svg[@class="lucide lucide-arrow-down-to-line"]')
-        
-        # # download_btn = driver.find_element(By.CSS_SELECTOR, "svg.lucide.lucide-arrow-down-to-line")
-        download_btn.click()
-
-# Need to open first and login to be prepared for new prompts
+# Open browser
 options = webdriver.ChromeOptions()
 options.headless = False
-
 driver = webdriver.Chrome(options=options)
-driver.get("https://discord.com/channels/991613372439212142/1169029179413643325")
-
-sleep(6*multiplier)
+driver.get("https://discord.com/channels/991613372439212142/1169029227123839028")
+sleep(6*sleep_factor)
 
 # buttons = driver.find_elements(By.TAG_NAME, "button")
 # buttons[1].click()
@@ -57,28 +50,10 @@ password_field.send_keys("2cyjcRN6&!TUfoum")
 buttons = driver.find_elements(By.TAG_NAME, "button")
 buttons[1].click()
 
-# sleep(9*multiplier)
+# sleep(9*sleep_factor)
 
-# Enter prompt
-
-# Manually click on something first and select "Trust lumalabs.ai links from now on"
+# !! TODO Currently manually click on an option first and select "Trust lumalabs.ai links from now on"
 
 while True:
     text = input()
     generate_asset(text)
-
-sleep(1*multiplier)
-
-# checkbox = driver.find_element(By.CLASS_NAME, "backdrop__7e89b withLayer__1fe9d")
-# checkbox = driver.find_element(By.TAG_NAME, "svg")
-# # checkbox.click()
-# # driver.execute_script("arguments[0].click();", checkbox)
-# webdriver.ActionChains(driver).move_to_element(checkbox).click(checkbox).perform()
-
-# buttons = driver.find_elements(By.TAG_NAME, "button")
-# buttons[0].click()
-
-# print(driver.page_source)
-
-sleep(100)
-driver.quit()
