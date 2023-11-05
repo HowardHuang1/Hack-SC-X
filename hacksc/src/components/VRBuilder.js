9154a97d57adbc9b39a510a675e05a5617b4f2ca
Howard Huang <howardhuang2004@gmail.com>
2023-11-05 10:34:06 -0800

    Update VRBuilder.js

--- hacksc/src/components/VRBuilder.js
+++ hacksc/src/components/VRBuilder.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import "./VRBuilder.css"
 import {
     Box,
@@ -15,7 +15,53 @@ import {
     Button,
   } from "@chakra-ui/react";
 
+import * as THREE from 'three';
+
 function VRBuilder() {
+    const ThreeScene = () => {
+        const scene = useRef(new THREE.Scene());
+        const camera = useRef(new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000));
+        const renderer = useRef(new THREE.WebGLRenderer());
+        
+        const mount = useRef(null);
+        
+        useEffect(() => {
+            const { current: cameraInstance } = camera;
+            const { current: rendererInstance } = renderer;
+            const { current: sceneInstance } = scene;
+        
+            rendererInstance.setSize(window.innerWidth, window.innerHeight);
+            mount.current.appendChild(rendererInstance.domElement);
+        
+            const geometry = new THREE.BoxGeometry();
+            const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+            const cube = new THREE.Mesh(geometry, material);
+            sceneInstance.add(cube);
+        
+            cameraInstance.position.z = 5;
+        
+            const animate = () => {
+            requestAnimationFrame(animate);
+            cube.rotation.x += 0.01;
+            cube.rotation.y += 0.01;
+            rendererInstance.render(sceneInstance, cameraInstance);
+            };
+        
+            animate();
+        }, []);
+        
+        return (
+            <div
+              style={{
+                width: '66.67%', // 2/3 of the page width
+                float: 'left',   // Left-aligned
+              }}
+              ref={mount}
+            />
+        );
+    };
+
+    ///////
     const [response, setResponse] = useState("");
     const [textPrompt, setTextPrompt] = useState('');
     const handleChange = (event) => {
@@ -31,7 +77,7 @@ function VRBuilder() {
   return (
     <div className="web-screen-partition">
         <div className="web-left-section">
-            left section
+            <ThreeScene />
         </div>
         <div className="web-right-section">
         <Accordion allowToggle allowMultiple w="100%">
